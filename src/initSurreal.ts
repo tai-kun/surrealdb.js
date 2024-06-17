c0c245104cb6af4bcaf1113c1d72b1bc9233f9a0
tai-kun <taikun.dev@gmail.com>
2024-06-17 11:15:47 +0900

    feat: SurrealQL のテンプレートリテラルを追加

--- src/initSurreal.ts
+++ src/initSurreal.ts
@@ -1,10 +1,24 @@
 import type { Constructor } from "type-fest";
+import { TypeError } from "./errors";
 import type { ClientConfig, default as ClientAbc } from "./models/ClientAbc";
+import type { RecordData } from "./types";
 
 type ClientConstructor = Constructor<
   any,
   ConstructorParameters<typeof ClientAbc>
 >;
+
+/**
+ * SurrealQL の初期化設定。
+ */
+export interface SurqlInit {
+  /**
+   * 変数のプレフィックス。
+   *
+   * @default "__js_tagged_"
+   */
+  readonly prefix?: string | undefined;
+}
 
 /**
  * Surreal の初期化設定。
@@ -18,6 +32,10 @@ export interface SurrealInit<T extends ClientConstructor = ClientConstructor>
    * クライアントのコンストラクター。
    */
   readonly Client: T;
+  /**
+   * SurrealQL の初期化設定。
+   */
+  readonly surql?: SurqlInit | undefined;
 }
 
 /**
@@ -27,6 +45,20 @@ export interface SurrealInit<T extends ClientConstructor = ClientConstructor>
  */
 export interface Surreal<T extends ClientConstructor> {
   new(): InstanceType<T> & AsyncDisposable;
+}
+
+export type RawSurql =
+  | string
+  | number
+  | bigint
+  | { readonly toSurql: () => string };
+
+class Raw<T extends RawSurql = RawSurql> {
+  constructor(public readonly value: T) {}
+}
+
+function raw<T extends RawSurql>(value: T): Raw<T> {
+  return new Raw<T>(value);
 }
 
 /**
@@ -40,13 +72,43 @@ export default function initSurreal<T extends ClientConstructor>(
   init: SurrealInit<T>,
 ): {
   Surreal: Surreal<T>;
+  Surql: {
+    new<T extends readonly unknown[] = unknown[]>(
+      text: string,
+      vars: RecordData,
+    ): {
+      readonly __type: T;
+      readonly text: string;
+      readonly vars: RecordData;
+    };
+  };
+  surql: {
+    <T extends readonly unknown[] = unknown[]>(
+      texts: readonly string[] | TemplateStringsArray,
+      ...values: unknown[]
+    ): {
+      readonly __type: T;
+      readonly text: string;
+      readonly vars: RecordData;
+    };
+    readonly raw: {
+      <T extends RawSurql>(value: T): {
+        readonly value: T;
+      };
+    };
+  };
 } {
   const {
+    surql: { prefix = "__js_tagged_" } = {},
     Client,
     engines,
     Formatter,
     Validator,
   } = init;
+
+  if (!/^[0-9a-z_]*$/i.test(prefix)) {
+    throw new TypeError(`Invalid prefix: ${prefix}`);
+  }
 
   // @ts-ignore
   class SurrealClass extends Client {
@@ -63,8 +125,67 @@ export default function initSurreal<T extends ClientConstructor>(
     }
   }
 
+  class Surql<T extends readonly unknown[] = unknown[]> {
+    /**
+     * @deprecated - この値は存在しません。型推論のためにのみ使用されます。
+     */
+    // @ts-expect-error
+    readonly __type: T;
+
+    readonly #text: string;
+
+    readonly #vars: RecordData;
+
+    constructor(text: string, vars: RecordData) {
+      this.#text = text;
+      this.#vars = vars;
+    }
+
+    get text() {
+      return this.#text;
+    }
+
+    get vars() {
+      return this.#vars;
+    }
+  }
+
+  function surql<T extends readonly unknown[] = unknown[]>(
+    texts: readonly string[] | TemplateStringsArray,
+    ...values: unknown[]
+  ): Surql<T> {
+    let text = "",
+      vars: Record<string, unknown> = {},
+      len = texts.length,
+      i = 0,
+      j: number;
+
+    for (; i < len; i++) {
+      text += texts[i];
+
+      if (i in values) {
+        if (values[i] instanceof Raw) {
+          const { value } = values[i] as Raw;
+          text += typeof value === "string"
+            ? value
+            : typeof value === "number" || typeof value === "bigint"
+            ? value.toString(10)
+            : value.toSurql();
+        } else {
+          j = values.indexOf(values[i]);
+          text += `$${prefix}${j}`;
+          vars[`${prefix}${j}`] = values[j];
+        }
+      }
+    }
+
+    return new Surql<T>(text, vars);
+  }
+
   return {
     // @ts-ignore
     Surreal: SurrealClass,
+    Surql,
+    surql: Object.assign(surql, { raw }),
   };
 }

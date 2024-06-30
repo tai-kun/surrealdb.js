6e75487d00b256474c185fdac4304b5c77f88d9c
tai-kun <taikun.dev@gmail.com>
2024-06-30 15:13:49 +0900

    fix: テストのスクリプトを修正

--- test-env/chrome/scripts/test.sh
+++ test-env/chrome/scripts/test.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
 
-c=0
+set -e
 
-while IFS= read -r -d '' file; do
-    echo
-    echo '================================================================================'
-    echo "= $file"
-    echo '================================================================================'
-    echo
-
-    # npx wtr "$file" --node-resolve --port 9000 --playwright --browsers chromium || c=$?
-    npx wtr "$file" || c=$?
-done < <(find tests -name '*.test.js' -print0)
-
-exit $c
+npx wtr 'tests/**/*.test.js'
--- test-env/firefox/scripts/test.sh
+++ test-env/firefox/scripts/test.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-c=0
+set -e
 
-while IFS= read -r -d '' file; do
-    echo
-    echo '================================================================================'
-    echo "= $file"
-    echo '================================================================================'
-    echo
-
-    node --enable-source-maps --test "$file" || c=$?
-done < <(find tests -name '*.test.js' -print0)
-
-exit $c
+npx wtr 'tests/**/*.test.js'

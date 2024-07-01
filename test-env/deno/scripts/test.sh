768024150e22cc81aa1b20184275780fe30d7fe7
tai-kun <taikun.dev@gmail.com>
2024-07-01 10:29:24 +0900

    chore(ci): テストできない箇所をジョブの段階でスキップ

--- test-env/deno/scripts/test.sh
+++ test-env/deno/scripts/test.sh
@@ -9,13 +9,6 @@ while IFS= read -r -d '' file; do
     echo '================================================================================'
     echo
 
-    if [[ "$file" == tests/medium/* ]]; then
-        echo '| 警告:'
-        echo '| Deno では全ミディアムテストがスキップされます。'
-
-        continue
-    fi
-
     deno test --allow-net --allow-env --allow-read --allow-run --trace-leaks "$file" || c=1
 done < <(find tests -name '*.test.js' -print0)
 

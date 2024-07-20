b8d6d01d2e51372fd51a91d1ed3aa379428b6b77
tai-kun <taikun.dev@gmail.com>
2024-07-20 09:23:50 +0900

    chore(values): シンボル名を変更

--- src/values/_shared/constants.ts
+++ src/values/_shared/constants.ts
@@ -1,15 +1,21 @@
-export const kDatetime = Symbol.for("surrealdb.Datetime");
-export const kDecimal = Symbol.for("surrealdb.Decimal");
-export const kDuration = Symbol.for("surrealdb.Duration");
-export const kGeometryPoint = Symbol.for("surrealdb.GeometryPoint");
-export const kGeometryLine = Symbol.for("surrealdb.GeometryLine");
-export const kGeometryPolygon = Symbol.for("surrealdb.GeometryPolygon");
-export const kGeometryMultiPoint = Symbol.for("surrealdb.GeometryMultiPoint");
-export const kGeometryMultiLine = Symbol.for("surrealdb.GeometryMultiLine");
+export const kDatetime = Symbol.for("@tai-kun/surreal/Datetime");
+export const kDecimal = Symbol.for("@tai-kun/surreal/Decimal");
+export const kDuration = Symbol.for("@tai-kun/surreal/Duration");
+export const kGeometryPoint = Symbol.for("@tai-kun/surreal/GeometryPoint");
+export const kGeometryLine = Symbol.for("@tai-kun/surreal/GeometryLine");
+export const kGeometryPolygon = Symbol.for("@tai-kun/surreal/GeometryPolygon");
+export const kGeometryMultiPoint = Symbol.for(
+  "@tai-kun/surreal/GeometryMultiPoint",
+);
+export const kGeometryMultiLine = Symbol.for(
+  "@tai-kun/surreal/GeometryMultiLine",
+);
 export const kGeometryMultiPolygon = Symbol.for(
   "surrealdb.GeometryMultiPolygon",
 );
-export const kGeometryCollection = Symbol.for("surrealdb.GeometryCollection");
-export const kTable = Symbol.for("surrealdb.Table");
-export const kThing = Symbol.for("surrealdb.Thing");
-export const kUuid = Symbol.for("surrealdb.Uuid");
+export const kGeometryCollection = Symbol.for(
+  "@tai-kun/surreal/GeometryCollection",
+);
+export const kTable = Symbol.for("@tai-kun/surreal/Table");
+export const kThing = Symbol.for("@tai-kun/surreal/Thing");
+export const kUuid = Symbol.for("@tai-kun/surreal/Uuid");

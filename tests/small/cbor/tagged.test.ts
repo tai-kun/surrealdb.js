1a8215c009437f2e3f5066bcc033000ebeca9500
tai-kun <taikun.dev@gmail.com>
2024-08-03 16:53:13 +0900

    test(tests/small/cbor): .toCBOR のテストを追加

--- tests/small/cbor/tagged.test.ts
+++ tests/small/cbor/tagged.test.ts
@@ -1,7 +1,10 @@
 import { decode, encode, Tagged } from "@tai-kun/surrealdb/cbor";
+import { SurrealTypeError } from "@tai-kun/surrealdb/errors";
 import { expect, test } from "vitest";
 
-const value = new Tagged(10, [new Tagged(11, 20)]);
+const value = new Tagged(10, [
+  new Tagged(11, 20),
+]);
 const bytes = new Uint8Array([
   0b110_01010, // mt: 6, ai: 10
   0b100_00001, // mt: 4, ai:  1
@@ -37,4 +40,42 @@ test("reviver", () => {
     }),
   )
     .toStrictEqual([20]);
+});
+
+test("toCBOR", () => {
+  const cborValue = {
+    toCBOR: () => [10, [
+      { toCBOR: () => [11, 20] },
+    ]],
+  };
+
+  expect(encode(cborValue)).toStrictEqual(bytes);
+});
+
+test("タグなし toCBOR", () => {
+  const cborValue = {
+    toCBOR: () => [20],
+  };
+
+  expect(encode(cborValue)).toStrictEqual(
+    new Uint8Array([
+      0b000_10100, // mt: 0, ai: 20
+    ]),
+  );
+});
+
+test("3 つ以上の要素を持つ配列を返してエラー", () => {
+  const cborValue = {
+    toCBOR: () => [0, 1, 2],
+  };
+
+  expect(() => encode(cborValue)).toThrowError(SurrealTypeError);
+});
+
+test("空配列を返してエラー", () => {
+  const cborValue = {
+    toCBOR: () => [],
+  };
+
+  expect(() => encode(cborValue)).toThrowError(SurrealTypeError);
 });

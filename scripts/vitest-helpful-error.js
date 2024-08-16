36d0d5ad9a0ee5e9f6996dd6bb16269013900f76
tai-kun <taikun.dev@gmail.com>
2024-08-16 15:37:03 +0900

    chore(scripts): global ではなく globalThis を使う

--- scripts/vitest-helpful-error.js
+++ scripts/vitest-helpful-error.js
@@ -2,15 +2,14 @@
 
 import { formatWithOptions } from "node-inspect-extracted";
 
-const OriginalError = global.Error;
+const OriginalError = globalThis.Error;
 
-global.Error.prototype.constructor = function(...args) {
+globalThis.Error.prototype.constructor = function(...args) {
   const this_ = OriginalError.prototype.constructor(...args); // super(...args);
 
   let formatted = null;
   let formatting = false;
   let originalName = this_.name;
-  let originalMessage = this_.message;
 
   Object.defineProperty(this_, "name", {
     set(name) {

8a931edba1be05cf772104bba316cd6b6ea4d207
tai-kun <taikun.dev@gmail.com>
2024-08-23 10:01:10 +0900

    docs: マルチバージョンに対応

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -27,14 +27,14 @@ export default defineConfig({
           },
           items: [
             {
-              slug: "getting-started",
+              slug: "v2/getting-started",
               label: "Getting Started",
               translations: {
                 ja: "はじめる",
               },
             },
             {
-              slug: "supports",
+              slug: "v2/supports",
               label: "Supported Environments",
               translations: {
                 ja: "サポートされている環境",
@@ -49,28 +49,28 @@ export default defineConfig({
           },
           items: [
             {
-              slug: "guides/connecting",
+              slug: "v2/guides/connecting",
               label: "Connecting",
               translations: {
                 ja: "接続",
               },
             },
             {
-              slug: "guides/authentication",
+              slug: "v2/guides/authentication",
               label: "Authentication",
               translations: {
                 ja: "認証",
               },
             },
             {
-              slug: "guides/querying",
+              slug: "v2/guides/querying",
               label: "Querying",
               translations: {
                 ja: "クエリー",
               },
             },
             {
-              slug: "guides/prepared-query",
+              slug: "v2/guides/prepared-query",
               label: "Prepared Query",
               translations: {
                 ja: "事前に定義されたクエリー",
@@ -85,21 +85,21 @@ export default defineConfig({
           },
           items: [
             {
-              slug: "experimental/inline-rpc",
+              slug: "v2/experimental/inline-rpc",
               label: "Inline RPC",
               translations: {
                 ja: "インライン RPC",
               },
             },
             {
-              slug: "experimental/inline-query",
+              slug: "v2/experimental/inline-query",
               label: "Inline Query",
               translations: {
                 ja: "インラインクエリー",
               },
             },
             {
-              slug: "experimental/auto-reconnect",
+              slug: "v2/experimental/auto-reconnect",
               label: "Auto Reconnect",
               translations: {
                 ja: "自動再接続",
@@ -117,23 +117,23 @@ export default defineConfig({
               label: "cbor",
               items: [
                 {
-                  slug: "reference/cbor/encode",
+                  slug: "v2/reference/cbor/encode",
                   label: "encode",
                 },
                 {
-                  slug: "reference/cbor/decode",
+                  slug: "v2/reference/cbor/decode",
                   label: "decode",
                 },
                 {
-                  slug: "reference/cbor/tagged",
+                  slug: "v2/reference/cbor/tagged",
                   label: "Tagged",
                 },
                 {
-                  slug: "reference/cbor/simple",
+                  slug: "v2/reference/cbor/simple",
                   label: "Simple",
                 },
                 {
-                  slug: "reference/cbor/others",
+                  slug: "v2/reference/cbor/others",
                   label: "Other tools",
                   translations: {
                     ja: "その他のツール",
@@ -145,61 +145,61 @@ export default defineConfig({
               label: "utils",
               items: [
                 {
-                  slug: "reference/utils/escape",
+                  slug: "v2/reference/utils/escape",
                   label: "escape",
                 },
                 {
-                  slug: "reference/utils/get-timeout-signal",
+                  slug: "v2/reference/utils/get-timeout-signal",
                   label: "getTimeoutSignal",
                 },
                 {
-                  slug: "reference/utils/is-array-buffer",
+                  slug: "v2/reference/utils/is-array-buffer",
                   label: "isArrayBuffer",
                 },
                 {
-                  slug: "reference/utils/is-browser",
+                  slug: "v2/reference/utils/is-browser",
                   label: "isBrowser",
                 },
                 {
-                  slug: "reference/utils/is-safe-number",
+                  slug: "v2/reference/utils/is-safe-number",
                   label: "isSafeNumber",
                 },
                 {
-                  slug: "reference/utils/make-abort-api",
+                  slug: "v2/reference/utils/make-abort-api",
                   label: "makeAbortApi",
                 },
                 {
-                  slug: "reference/utils/mutex",
+                  slug: "v2/reference/utils/mutex",
                   label: "mutex",
                 },
                 {
-                  slug: "reference/utils/serial",
+                  slug: "v2/reference/utils/serial",
                   label: "Serial",
                 },
                 {
-                  slug: "reference/utils/stateful-promise",
+                  slug: "v2/reference/utils/stateful-promise",
                   label: "StatefulPromise",
                 },
                 {
-                  slug: "reference/utils/task-queue",
+                  slug: "v2/reference/utils/task-queue",
                   label: "TaskQueue",
                 },
                 {
-                  slug: "reference/utils/task-emitter",
+                  slug: "v2/reference/utils/task-emitter",
                   label: "TaskEmitter",
                 },
                 {
-                  slug: "reference/utils/throw-if-aborted",
+                  slug: "v2/reference/utils/throw-if-aborted",
                   label: "throwIfAborted",
                 },
                 {
-                  slug: "reference/utils/to-surql",
+                  slug: "v2/reference/utils/to-surql",
                   label: "toSurql",
                 },
               ],
             },
             {
-              slug: "reference/errors",
+              slug: "v2/reference/errors",
               label: "Errors",
               translations: {
                 ja: "エラー",

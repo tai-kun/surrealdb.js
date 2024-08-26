21b0a943708bdbcd51a2ecdfc84c24790eebb6bd
tai-kun <taikun.dev@gmail.com>
2024-08-27 07:12:43 +0900

    chore(scripts): 利用可能な surreal を追加

--- scripts/pretest/setup_surrealdb.sh
+++ scripts/pretest/setup_surrealdb.sh
@@ -9,15 +9,31 @@ case "$surrealdb_version" in
 latest)
     surrealdb_install_options=""
     ;;
+nightly)
+    surrealdb_install_options="--nightly"
+    ;;
 alpha)
     surrealdb_install_options="--alpha"
     ;;
 beta)
     surrealdb_install_options="--beta"
     ;;
+local)
+    surrealdb_install_options="LOCAL"
+    ;;
 *)
     surrealdb_install_options="--version $surrealdb_version"
     ;;
 esac
 
-curl -LsSf https://install.surrealdb.com | sh -s -- $surrealdb_install_options
+if [ "$surrealdb_install_options" != "LOCAL" ]; then
+    curl -LsSf https://install.surrealdb.com |
+        sh -s -- $surrealdb_install_options
+elif [ -f ./surreal ]; then
+    chmod +x surreal
+    mv surreal /usr/local/bin/
+else
+    :
+fi
+
+which surreal

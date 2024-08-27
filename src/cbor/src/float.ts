5c42621ed395914933b41ed48d7a2bbeb3aa7079
tai-kun <taikun.dev@gmail.com>
2024-08-27 11:49:43 +0900

    chore(cbor): float16 の取得を RFC に記載されている方法に変更

--- src/cbor/src/float.ts
+++ src/cbor/src/float.ts
@@ -1,27 +1,23 @@
-import { getFloat16 as get } from "@petamoriken/float16";
-
 export function getFloat16(view: DataView, offset: number): number {
-  return get(view, offset);
-
   // https://www.rfc-editor.org/rfc/rfc8949.html#name-half-precision
   // の JavaScript バージョン:
 
-  // function ldexp(mantissa: number, exponent: number): number {
-  //   return mantissa * Math.pow(2, exponent);
-  // }
+  function ldexp(mantissa: number, exponent: number): number {
+    return mantissa * Math.pow(2, exponent);
+  }
 
-  // const half = (view[offset]! << 8) + bytes[offset + 1]!;
-  // const exp = (half >> 10) & 0x1f;
-  // const mant = half & 0x3ff;
-  // const value = exp === 0
-  //   ? ldexp(mant, -24)
-  //   : exp !== 31
-  //   ? ldexp(mant + 1024, exp - 25)
-  //   : mant === 0
-  //   ? Infinity
-  //   : NaN;
+  const half = (view.getUint8(offset) << 8) + view.getUint8(offset + 1);
+  const exp = (half >> 10) & 0x1f;
+  const mant = half & 0x3ff;
+  const value = exp === 0
+    ? ldexp(mant, -24)
+    : exp !== 31
+    ? ldexp(mant + 1024, exp - 25)
+    : mant === 0
+    ? Infinity
+    : NaN;
 
-  // return half & 0x8000
-  //   ? -value
-  //   : value;
+  return half & 0x8000
+    ? -value
+    : value;
 }
